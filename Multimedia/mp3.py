1607b9f276f8fb327c1b863f1add452c5a3be3c3
PIUphil <ahnkp08@daum.net>
2021-08-05 18:14:31 +0900

    mp3

--- Multimedia/mp3.py
+++ Multimedia/mp3.py
@@ -1,9 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
-from PyQt5.QtCore import QDir, QUrl
-
-, QtWidgets, QtMultimedia
+from PyQt5.QtCore import QDir, QUrl, QtWidgets, QtMultimedia
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

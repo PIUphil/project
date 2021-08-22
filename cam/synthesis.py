cba4da792daeb02e3008f1c45e06c1d853203f20
PIUphil <ahnkp08@daum.net>
2021-08-22 11:43:40 +0900

    cam

--- cam/synthesis.py
+++ cam/synthesis.py
@@ -1,38 +1,28 @@
 import cv2
-from PIL import Image
-import numpy as np
 
-src = cv2.imread('pic.png')
-mask = cv2.imread('makeMask.png')#, cv2.IMREAD_COLOR)
-# maskg = cv2.imread('makeMask.png', cv2.IMREAD_GRAYSCALE)
-dst = cv2.imread('tomb2.png')
+src = cv2.imread('pic.png')             # 원본사진
+mask = cv2.imread('makeMask.png')       # 마스크이미지 - imgcut.py
+dst = cv2.imread('tomb2.png')           # 배경1
+dst2 = cv2.imread('crown.png')          # 배경2
 
-# # img = Image.open('makeMask.png')
-# maskGray = mask.convert('L')
-# maskGray.save('makeMask_gray.png')
+# 사이즈 변경 
+src = cv2.resize(src, (1024, 576))
+mask = cv2.resize(mask, (1024, 576))
+dst = cv2.resize(dst, (1024, 576))
+dst2 = cv2.resize(dst2, (1024, 576))
 
-# cv2.imwrite('makeMask_gray.png',mask)
-# maskgray = cv2.imread('makeMask_gray.png', cv2.IMREAD_GRAYSCALE)
+# mask_hsv = cv2.cvtColor(mask, cv2.COLOR_BGR2HSV)        # 색채변환(hsv)
+# maskg = cv2.cvtColor(mask_hsv, cv2.COLOR_BGR2GRAY)      # 흑백처리
 
+# cv2.imwrite('makeMaskHsv_gray.png',maskg*1.5)           # 대비값 증가
 
-mask_hsv = cv2.cvtColor(mask, cv2.COLOR_BGR2HSV)
+# maskgg = cv2.imread('makeMaskHsv_gray.png', cv2.IMREAD_GRAYSCALE)
 
-maskg = cv2.cvtColor(mask_hsv, cv2.COLOR_BGR2GRAY)
+cv2.copyTo(src, mask, dst)
 
-
-cv2.imwrite('makeMaskHsv_gray.png',maskg*1.2)
-
-maskgg = cv2.imread('makeMaskHsv_gray.png', cv2.IMREAD_GRAYSCALE)
-
-cv2.copyTo(src, maskgg, dst)
-
-# cv2.imshow('mask_blue', mask_blue)
-cv2.imshow('src',src)
-# cv2.imshow('mask',mask)
+# cv2.imshow('src',src)
+# cv2.imshow('mask', mask)
 cv2.imshow('dst', dst)
-cv2.imshow('maskgg', maskgg)
-# cv2.imshow('hsv', hsv)
-# cv2.imshow('src', src)
 
 
 cv2.waitKey()
